cda96e764fdf1f8008f6d772af17a635176718f2
Guilherme Nunes <guilherme.augd@gmail.com>
2025-10-21 16:27:16 -0300

    reset db credentials

--- src/main/java/org/fiap/factory/ConnectionFactory.java
+++ src/main/java/org/fiap/factory/ConnectionFactory.java
@@ -4,8 +4,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 public class ConnectionFactory {
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    private static final String USUARIO = "RM562938";
-    private static final String SENHA = "250191";
+    private static final String USUARIO = "RM00000";
+    private static final String SENHA = "000000";
     // Método para obter uma conexão com o banco de dados
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
